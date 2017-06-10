57ef53fd421f0f44d396fcddc9f8beab765f61cf
Koji <keroromu@gmail.com>
2017-06-11 04:11:30 +0900

    minor update

--- testdata.php
+++ testdata.php
@@ -10,6 +10,13 @@ $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1))
 $pdo = new PDO($dsn, $url['user'], $url['pass']);
 echo "<p>".$pdo->getAttribute(PDO::ATTR_SERVER_VERSION)."</p>";
 //var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+$sql = "CREATE TABLE IF NOT EXISTS `FIRST_TABLE`"
+."("
+. "`color` text,"
+. "`playlists` text,"
+.");";
+$stmt = $pdo -> prepare($sql);
+$stmt -> execute();
 ?>
 </body>
 </html>
