51a221d942e4c743c4151981f0acabd36dabdbb4
Koji <keroromu@gmail.com>
2017-06-11 03:57:47 +0900

    minor update

--- testdata.php
+++ testdata.php
@@ -5,11 +5,10 @@
 </head>
 <body style="background-color : #BDBDBD">
 <?php
-// $url = parse_url(getenv('DATABASE_URL'));
-// $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
-// $pdo = new PDO($dsn, $url['user'], $url['pass']);
-ehco "<p>hello</p>";
-// ehco "<p>" + $pdo->getAttribute(PDO::ATTR_SERVER_VERSION) + "</p>";
+$url = parse_url(getenv('DATABASE_URL'));
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+$pdo = new PDO($dsn, $url['user'], $url['pass']);
+echo "<p>".$pdo->getAttribute(PDO::ATTR_SERVER_VERSION)."</p>";
 //var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 ?>
 </body>
