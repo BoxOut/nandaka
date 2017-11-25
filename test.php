e3870557e4baceeebe22d63416a1a86cb0d4664f
ubuntu64 <koji@dell>
2017-11-25 06:18:14 -0800

    bugfix on testing database

--- test.php
+++ test.php
@@ -2,13 +2,10 @@
 	$dbopts = parse_url(getenv('DATABASE_URL'));
 	echo 'hello';
 	print_r($dbopts);
-	echo  "host ".$dbopts["host"].'\n';
-	echo  "port ".$dbopts["port"].'\n';
-	echo  "pass ".$dbopts["pass"].'\n';
-	echo  "path ".$dbopts["path"].'\n';
+
 	try {
 		// 各種パラメータを指定して接続
-		$pdo_conn = new PDO( 'pgsql:host='.$dbopts["host"].'; dbname='.$dbopts["path"], $dbopts["user"], ltrim($dbopts["path"],'/') );
+		$pdo_conn = new PDO( 'pgsql:host='.$dbopts["host"].'; dbname='.ltrim($dbopts["path"],'/'), $dbopts["user"], $dbopts["pass"] );
 		var_dump("接続に成功しました");
 	}
 	catch(PDOException $e) {
