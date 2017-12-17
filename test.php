d339c347f563610a346b599fea54f4ee01a41724
ubuntu64 <koji@dell>
2017-12-17 01:49:18 -0800

    minor update on database

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php
 	$dbopts = parse_url(getenv('DATABASE_URL'));
-	echo 'hello';
+	echo 'hello\n';
 	// print_r($dbopts);
 
 	try {
 		// 各種パラメータを指定して接続
 		$pdo_conn = new PDO( 'pgsql:host='.$dbopts["host"].'; dbname='.ltrim($dbopts["path"],'/'), $dbopts["user"], $dbopts["pass"] );
-		var_dump("接続に成功しました");
+		var_dump("接続に成功しました\n");
 	}
 	catch(PDOException $e) {
 	var_dump($e->getMessage());
@@ -14,6 +14,6 @@
 	$sql = "SELECT * FROM test";
 	$res = $pdo_conn->query($sql);
 	foreach( $res as $value ) {
-		echo "$value[name]<br>";
+		echo "$value[name] $value[id] <br>";
 	}
 ?>
