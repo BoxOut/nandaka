e30a7343cf3433b2a9a76061930bba1e8c8f93ab
ubuntu64 <koji@dell>
2017-12-24 05:53:12 -0800

    minor update on database

---
+++ database_handler.php
@@ -0,0 +1,31 @@
+<?php
+
+	$dbopts = parse_url(getenv('DATABASE_URL'));
+	header('Content-Type: application/json');
+
+	try {
+		// 各種パラメータを指定して接続
+		$pdo = new PDO( 'pgsql:host='.$dbopts["host"].'; dbname='.ltrim($dbopts["path"],'/'), $dbopts["user"], $dbopts["pass"] );
+		var_dump("接続に成功しました\n");
+	}
+	catch(PDOException $e) {
+	var_dump($e->getMessage());
+	}
+	if($_POST['op_type']=='add'){
+		$stmt = $pdo->prepare('insert into playlist VALUES (?, ?, ?)');
+		$stmt->bindValue(1, $_POST['video_id']);
+		$stmt->bindValue(1, $_POST['video_title']);
+		$stmt->bindValue(2, $_POST['img_id']);
+		$stmt->execute();
+	}
+	else {
+		$sql = "SELECT * FROM playlist";
+		$res = $pdo->query($sql);
+		$list = "{";
+		foreach ($data as $res) {
+			$list += "$_POST['age']},{$_POST['job']";
+		}
+		$list += "}";
+    	echo json_encode(compact('list'));
+	}
+?>
